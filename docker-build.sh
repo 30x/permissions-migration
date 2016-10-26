7071f73c7caa7352ada49493aa7d7fbb6d91ed17
Martin Nally <mnally@apigee.com>
2016-10-25 19:13:30 -0700

    bump versions

--- docker-build.sh
+++ docker-build.sh
@@ -1,2 +1,2 @@
 docker build -t permissions-migration .
-docker tag -f permissions-migration thirtyx/permissions-migration:0.0.5
+docker tag -f permissions-migration thirtyx/permissions-migration:0.0.6
--- docker-push.sh
+++ docker-push.sh
@@ -1,1 +1,1 @@
-docker push thirtyx/permissions-migration:0.0.5
+docker push thirtyx/permissions-migration:0.0.6
